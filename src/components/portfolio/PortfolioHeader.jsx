d0447f002cea618633f3661240660eb92b19eebc
Thirosh Madhusha <thiroshmadhusha0520@gmail.com>
2023-01-13 23:12:41 +0530

    Update and add my new portfolio section option with react card slicker

---
+++ src/components/portfolio/PortfolioHeader.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./portfolio.css";
+
+const PortfolioHeader = () => {
+  return (
+    <section id="portfolio">
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+    </section>
+  );
+}
+
+export default PortfolioHeader
---
+++ src/components/portfolioslider/PortfolioSlider.jsx
@@ -0,0 +1,37 @@
+import Card from "react-bootstrap/Card";
+import "./card.css";
+
+const PortfolioSlider = ({ item }) => {
+  return (
+    <Card
+      style={{
+        width: "18rem",
+        borderRadius: "2rem",
+        backgroundColor: "#170d41",
+        marginLeft: "auto",
+        marginRight: "auto",
+      }}
+    >
+      <div className="item-con" key={item.id}>
+        <div className="item-container">
+          <img src={item.image} alt="portfolio project image" />
+          <h2>{item.title} </h2>
+          <p>{item.description}</p>
+
+          {/* new */}
+          <div className="git-btn">
+            <a
+              href={item.github}
+              className="gitbtn"
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              GitHub
+            </a>
+          </div>
+        </div>
+      </div>
+    </Card>
+  );
+};
+export default PortfolioSlider;
