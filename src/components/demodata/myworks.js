a7edd48dda799e216dafb00b0bfa1b6972cef4a1
Thirosh Madhusha <thiroshmadhusha0520@gmail.com>
2023-01-13 23:10:43 +0530

    add demo data for portfolio section

---
+++ src/components/demodata/myworks.js
@@ -0,0 +1,131 @@
+import img1 from "../../img/img1.jpg";
+import img2 from "../../img/img2.jpg";
+import img3 from "../../img/img3.jpg";
+import img4 from "../../img/img4.jpg";
+import img5 from "../../img/img5.jpg";
+
+export const frontend = [
+  {
+    id: 1,
+    image: img1,
+    title: "Online ebay Shopping System ",
+    category: "Java",
+    description: "Lorem ipsum dolor sit amet,",
+    github: "",
+  },
+  {
+    id: 2,
+    image: img2,
+    title: "Tea Factory Management",
+    category: "Java",
+    description: "Lorem ipsum dolor sit amet,",
+    github: "",
+  },
+  {
+    id: 3,
+    image: img3,
+    title: "Tips and tricks to write goos ",
+    category: "Java",
+    description: "Lorem ipsum dolor sit amet,",
+    github: "",
+  },
+  {
+    id: 4,
+    image: img4,
+    title: "Tips and tricks to write good",
+    category: "Java",
+    description: "Lorem ipsum dolor sit amet,",
+    github: "",
+  },
+  {
+    id: 5,
+    image: img5,
+    title: "Tips and tricks to write good",
+    category: "Java",
+    description: "Lorem ipsum dolor sit amet,",
+    github: "",
+  },
+  {
+    id: 6,
+    image: img1,
+    title: "Tips and tricks to write good",
+    category: "Java",
+    description: "Lorem ipsum dolor sit amet,",
+    github: "",
+  },
+  {
+    id: 7,
+    image: img2,
+    title: "Tips and tricks to write good",
+    category: "Java",
+    description: "Lorem ipsum dolor sit amet,",
+    github: "",
+  },
+  {
+    id: 8,
+    image: img3,
+    title: "Tips and tricks to write good",
+    category: "Java",
+    description: "Lorem ipsum dolor sit amet,",
+    github: "",
+  },
+  {
+    id: 9,
+    image: img4,
+    title: "Tips and tricks to write good",
+    category: "Java",
+    description: "Lorem ipsum dolor sit amet,",
+    github: "",
+  },
+  {
+    id: 10,
+    image: img5,
+    title: "Tips and tricks to write good",
+    category: "Java",
+    description: "Lorem ipsum dolor sit amet,",
+    github: "",
+  },
+];
+
+export const backend = [
+  {
+    id: 1,
+    image: img1,
+    title: "Online ebay Shopping System ",
+    category: "Java",
+    description: "Lorem ipsum dolor sit amet,",
+    github: "",
+  },
+  {
+    id: 5,
+    image: img5,
+    title: "Tips and tricks to write good",
+    category: "Java",
+    description: "Lorem ipsum dolor sit amet,",
+    github: "",
+  },
+  {
+    id: 10,
+    image: img5,
+    title: "Tips and tricks to write good",
+    category: "Java",
+    description: "Lorem ipsum dolor sit amet,",
+    github: "",
+  },
+  {
+    id: 5,
+    image: img5,
+    title: "Tips and tricks to write good",
+    category: "Java",
+    description: "Lorem ipsum dolor sit amet,",
+    github: "",
+  },
+  {
+    id: 10,
+    image: img5,
+    title: "Tips and tricks to write good",
+    category: "Java",
+    description: "Lorem ipsum dolor sit amet,",
+    github: "",
+  },
+];
